9139e3f6787df8032f72d23ce6511a1c9124a204
Ricardo Otero <oterosantos@gmail.com>
2025-05-27 01:32:39 +0100

    Remove unused includes

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,4 @@
 #include "utils.h"
-#include "mino_grid.h"
-#include "playfield.h"
-#include "tetrimino.h"
 
 void must_init(bool test, const char *description) {
   if (test) {
--- src/utils.h
+++ src/utils.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "playfield.h"
-#include "tetrimino.h"
 #include <stdio.h>
 #include <stdlib.h>
 
